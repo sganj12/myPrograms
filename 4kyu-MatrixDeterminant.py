c5b95e74867ac15adc2bc653577607661185c304
Soumyadip <soumyadipganguly@ymail.com>
2018-12-12 00:45:25 -0500

    updating my additions

---
+++ 4kyu-MatrixDeterminant.py
@@ -0,0 +1,11 @@
+def determinant(matrix):
+    mlen = len(matrix)
+    if mlen==1: return matrix[0]
+    elif mlen==2: return matrix[0][0]*matrix[1][1] - matrix[1][0]*matrix[0][1]
+    else:
+        smat =
+
+
+
+print(determinant([[4,6],[3,8]]))
+
--- info.md
+++
@@ -1,3 +0,0 @@
-#readme
-
-Learning GIT
